13ea2c85db8df90ed7a5f20f6fd1c3722c6285db
Nathan Isom <nathanisom27@gmail.com>
2016-04-06 11:54:13 -0500

    spoiler patch

--- st.c
+++ st.c
@@ -3809,9 +3809,14 @@ xdrawglyphfontspecs(const XftGlyphFontSpec *specs, Glyph base, int len, int x, i
 	}
 
 	if (base.mode & ATTR_REVERSE) {
-		temp = fg;
-		fg = bg;
-		bg = temp;
+		if (bg == fg) {
+			bg = &dc.col[defaultfg];
+			fg = &dc.col[defaultbg];
+		} else {
+			temp = fg;
+			fg = bg;
+			bg = temp;
+		}
 	}
 
 	if ((base.mode & ATTR_BOLD_FAINT) == ATTR_FAINT) {
