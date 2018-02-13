c6645653b13a7af28db6b2950c6c261d6d2f484b
Michal Koutenský <koutak.m@gmail.com>
2018-02-13 21:55:08 +0100

    allow overriding colors in the 256 color space

--- src/st.c
+++ src/st.c
@@ -3445,7 +3445,7 @@ xloadcolor(int i, const char *name, Color *ncolor)
 	XRenderColor color = { .alpha = 0xffff };
 
 	if (!name) {
-		if (BETWEEN(i, 16, 255)) { /* 256 color */
+		if (!colorname[i] && BETWEEN(i, 16, 255)) { /* 256 color */
 			if (i < 6*6*6+16) { /* same colors as xterm */
 				color.red   = sixd_to_16bit( ((i-16)/36)%6 );
 				color.green = sixd_to_16bit( ((i-16)/6) %6 );
