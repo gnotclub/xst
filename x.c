d143be08a50b70e66dad45a79e20b934d4b78da4
neeasade <neeasade@gmail.com>
2020-07-03 14:15:01 -0500

    st -> xst

--- x.c
+++ x.c
@@ -2172,7 +2172,7 @@ run:
 		opt_cmd = argv;
 
 	if (!opt_title)
-		opt_title = (opt_line || !opt_cmd) ? "st" : opt_cmd[0];
+		opt_title = (opt_line || !opt_cmd) ? "xst" : opt_cmd[0];
 
 	setlocale(LC_CTYPE, "");
 	XSetLocaleModifiers("");
