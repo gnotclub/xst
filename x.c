a0894b52af4ee6cba42e93a2a756a2092ee46e48
Yauhen Kirylau <actionless.loveless@gmail.com>
2020-12-11 22:41:07 +0100

    fix(x: main): do xrdb_load before setting cursor shape (fixes #127) (#129)
    * fix(x: main): do xrdb_load before setting cursor shape (fixes #127)
    
    * fix(x: main): do xrdb_load and xsetcursor after parsing cli args

--- x.c
+++ x.c
@@ -2111,7 +2111,6 @@ main(int argc, char *argv[])
 {
 	xw.l = xw.t = 0;
 	xw.isfixed = False;
-	xsetcursor(cursorshape);
 
 	ARGBEGIN {
 	case 'a':
@@ -2167,12 +2166,13 @@ run:
 	if (!opt_title)
 		opt_title = (opt_line || !opt_cmd) ? "xst" : opt_cmd[0];
 
+	xrdb_load();
+	xsetcursor(cursorshape);
 	setlocale(LC_CTYPE, "");
 	XSetLocaleModifiers("");
 	cols = MAX(cols, 1);
 	rows = MAX(rows, 1);
 	tnew(cols, rows);
-	xrdb_load();
 	xinit(cols, rows);
 	xsetenv();
 	selinit();
