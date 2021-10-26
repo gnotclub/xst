3ee464a27caa1942c4161bccdd01e4acb9e57c47
strikerlulu <strikerlulu7@gmail.com>
2021-10-26 15:46:06 +0530

    Fix externalpipe cmd for launching url in browser

--- config.def.h
+++ config.def.h
@@ -232,6 +232,10 @@ static MouseShortcut mshortcuts[] = {
 	{ XK_ANY_MOD,           Button5, ttysend,        {.s = "\005"} },
 };
 
+/* Command for opening url in browser */
+static char *openurlcmd[] = {"/bin/sh", "-c", "xurls | eval dmenu $(dmenu_options) | xargs -r $BROWSER",
+                             "externalpipe", NULL};
+
 /* Internal keyboard shortcuts. */
 #define MODKEY Mod1Mask
 #define TERMMOD (ControlMask|ShiftMask)
@@ -252,7 +256,7 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
 	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
 	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
-	{ MODKEY,               'u',            externalpipe,   {.v = "xurls | eval dmenu $(dmenu_options) | xargs -r $BROWSER" } },
+	{ MODKEY,               'u',            externalpipe,   {.v = openurlcmd } },
 };
 
 /*
