d4d882e9a6f8d0dfaf57252fcf6459a3bc7dd265
Raafat Turki <raafat.turki@protonmail.com>
2021-11-30 20:22:42 +0300

    wip: patching undercurls

--- st.h
+++ st.h
@@ -35,6 +35,7 @@ enum glyph_attribute {
 	ATTR_WDUMMY     = 1 << 10,
 	ATTR_BOXDRAW    = 1 << 11,
 	ATTR_BOLD_FAINT = ATTR_BOLD | ATTR_FAINT,
+ 	ATTR_DIRTYUNDERLINE = 1 << 15,
 };
 
 enum selection_mode {
@@ -66,6 +67,8 @@ typedef struct {
 	ushort mode;      /* attribute flags */
 	uint32_t fg;      /* foreground  */
 	uint32_t bg;      /* background  */
+    int ustyle;	      /* underline style */
+    int ucolor[3];    /* underline color */
 } Glyph;
 
 typedef Glyph *Line;
