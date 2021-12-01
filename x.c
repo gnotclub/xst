02d99463b2c04fc3b4863c2de5eca99a7b12ea28
Raafat Turki <raafat.turki@protonmail.com>
2021-12-02 01:35:22 +0300

    fix: added y offset to the undercurl wave

--- x.c
+++ x.c
@@ -1761,7 +1761,7 @@ xdrawglyphfontspecs(const XftGlyphFontSpec *specs, Glyph base, int len, int x, i
             int ww = win.cw;//width;
             int wh = dc.font.descent - wlw/2 - 1;//r.height/7;
             int wx = winx;
-            int wy = winy + win.ch - dc.font.descent;
+            int wy = winy + win.cyo + win.ch - dc.font.descent;
 
             // Cap is half of wave width
             float capRatio = 0.5f;
