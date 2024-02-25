3a61f5495651613e4fb1f1251613e35dc1e211e4
Quentin Rameau <quinq@fifth.space>
2024-02-25 01:31:31 +0100

    Fix cursor move with wide glyphs
    st would always move back 1 column,
    even with wide glyhps (using more than a single column).
    
    The glyph rune is set on its first column,
    and the other ones are to 0,
    so loop until we detect the start of the previous glyph.

--- st.c
+++ st.c
@@ -91,8 +91,8 @@ enum escape_state {
 
 typedef struct {
 	Glyph attr; /* current char attributes */
-	int x;
-	int y;
+	int x; /* terminal column */
+	int y; /* terminal row */
 	char state;
 } TCursor;
 
@@ -2358,12 +2358,16 @@ tstrsequence(uchar c)
 void
 tcontrolcode(uchar ascii)
 {
+	size_t i;
+
 	switch (ascii) {
 	case '\t':   /* HT */
 		tputtab(1);
 		return;
 	case '\b':   /* BS */
-		tmoveto(term.c.x-1, term.c.y);
+		for (i = 1; term.c.x && term.line[term.c.y][term.c.x - i].u == 0; ++i)
+			;
+		tmoveto(term.c.x - i, term.c.y);
 		return;
 	case '\r':   /* CR */
 		tmoveto(0, term.c.y);
