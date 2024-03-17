1a8cdf13a8a56c5074ce4003b501cc7ca430a85b
Hiltjo Posthuma <hiltjo@codemadness.org>
2024-03-17 14:42:44 +0100

    config.def.h: improve latency for the default configuration

--- config.def.h
+++ config.def.h
@@ -83,7 +83,7 @@ int allowwindowops = 0;
  * near minlatency, but it waits longer for slow updates to avoid partial draw.
  * low minlatency will tear/flicker more, as it can "detect" idle too early.
  */
-static double minlatency = 8;
+static double minlatency = 2;
 static double maxlatency = 33;
 
 /*
